64a2bfef4d5f8a1bd0063d6da8e9f374caca75c0
PizzzaDog <7883494@gmail.com>
2021-04-05 21:43:03 +0300

    added some files

---
+++ src/lesson1/Dog.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class Dog {
+    String name;
+    int age;
+
+    public void speak() {
+        System.out.println("Привет, меня зовут " + name + " и мне столько лет: " + age);
+    }
+
+}
---
+++ src/lesson1/Main.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Dog dog1 = new Dog();
+        dog1.name = "Hatiko";
+        dog1.age = 10;
+        dog1.speak();
+    }
+}
